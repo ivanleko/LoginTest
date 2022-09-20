1b6e3087cf5dede460798a045f121fa25cb1d604
Ivan Leko <ivanleko@Ivans-MacBook-Pro.local>
2022-09-20 09:52:32 +0200

    init

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, KeyboardAvoidingView } from 'react-native';
+import LoginScreen from "./react-native-login-screen/lib/LoginScreen";
+import SocialButton from "./react-native-login-screen/lib/components/social-button/SocialButton";
+import TextInput from "react-native-text-input-interactive";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    < KeyboardAvoidingView style={styles.container}>
+      <Image resizeMode="contain" style={[styles.logoImageStyle]}
+        source={require("./luckauprava.png")}/>
+      <LoginScreen style={styles.container}  loginButtonStyle={styles.novi} disableSocialButtons disableSignup disableDivider />
       <StatusBar style="auto" />
-    </View>
+    </ KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#8BAEDC",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  }, 
+  logoImageStyle: {
+    flex: 2,
+    width: "100%",
+    resizeMode: "contain",
+    backgroundColor: "red"
   },
+  LuckaUpravaLogo: {
+    flex: 2,
+    width: "100%",
+    resizeMode: "contain",
+    flexDirection: "column",
+   
+    backgroundColor: "#8BAEDC"
+}, novi: {
+  backgroundColor: "#705BA7",
+  height: 60
+}
 });
